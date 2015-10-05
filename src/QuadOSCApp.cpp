dbcd19bed7d89df4c6ec6d06dce13fa6725f0e42
Nicholas Bulka <nicholasbulka@gmail.com>
2015-10-04 22:11:23 -0400

    AT mode works with freeStream xml file

--- src/QuadOSCApp.cpp
+++ src/QuadOSCApp.cpp
@@ -1,28 +1,90 @@
 #include "cinder/app/AppNative.h"
 #include "cinder/gl/gl.h"
+#include "OscListener.h"
+#include "OscSender.h"
+#include "cinder/Serial.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/Text.h"
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
+#define BUFSIZE 80
+#define READ_INTERVAL 5.25
+
 class QuadOSCApp : public AppNative {
   public:
 	void setup();
-	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+    
+    osc::Listener mOscListener;
+    Vec2f mOscPosition;
+    
+    Serial serial;
+    uint8_t			ctr;
+    
+    double sinceLastRead, lastUpdate;
+    
 };
 
 void QuadOSCApp::setup()
 {
-}
+    mOscListener.setup(8001);
+    
+    ctr = 78;
 
-void QuadOSCApp::mouseDown( MouseEvent event )
-{
+    sinceLastRead = 0.0;
+    lastUpdate = 0.0;
+    
+    // print the devices
+    const vector<Serial::Device> &devices( Serial::getDevices() );
+    for( vector<Serial::Device>::const_iterator deviceIt = devices.begin(); deviceIt != devices.end(); ++deviceIt ) {
+        console() << "Device: " << deviceIt->getName() << endl;
+    }
+    
+    try {
+        Serial::Device dev = Serial::findDeviceByNameContains("tty.usbserial");
+        serial = Serial( dev, 9600);
+        console() << "the try fired" << std::endl;
+    }
+    catch( ... ) {
+        console() << "There was an error initializing the serial device!" << std::endl;
+        exit( -1 );
+    }
 }
 
 void QuadOSCApp::update()
 {
+    
+    //serial.writeByte(ctr);
+    
+    console() << ctr << std::endl;
+    while (mOscListener.hasWaitingMessages())
+    {
+        osc::Message msg;
+        mOscListener.getNextMessage(&msg);
+        
+        //	console() << "Bytes available: " << serial.getNumBytesAvailable() << std::endl;
+        
+        double now = getElapsedSeconds();
+        double deltaTime = now - lastUpdate;
+        lastUpdate = now;
+        sinceLastRead += deltaTime;
+        
+        if(sinceLastRead > READ_INTERVAL)
+        {
+            /*reset the last read time */
+            
+            sinceLastRead = 0.0;
+            
+            /* Serial data written here */
+            
+            serial.writeByte(ctr);
+        }
+
+    }
 }
 
 void QuadOSCApp::draw()
