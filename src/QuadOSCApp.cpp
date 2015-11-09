dd48c561e776cb718ba19295495cce499ff13e9e
Nicholas Bulka <nicholasbulka@gmail.com>
2015-11-08 19:47:18 -0500

    adding arduino directory

--- src/QuadOSCApp.cpp
+++ src/QuadOSCApp.cpp
@@ -15,7 +15,7 @@ using namespace ci;
 using namespace ci::app;
 using namespace std;
 
-#define READ_INTERVAL 1.5
+#define READ_INTERVAL .01
 
 // #define TEST_PACKET 7E 00 0E 00 01 00 7D 33 A2 00 40 C3 44 DA 00 (6E 4E 6E) FE
 // parentheses enclose the real message (RF Data)
@@ -31,10 +31,9 @@ class QuadOSCApp : public AppNative {
     
     Serial serial;
     
-     uint8_t outBuffer[61] {126,0,56,0,1,0,125,51,162,0,64,195,68,218,0,60,119,62,48,46,48,56,55,52,54,53,60,47,119,62,60,120,62,48,46,48,56,55,52,54,53,60,47,120,62,60,121,62,48,46,48,56,55,52,54,53,60,47,121,62, 27};
+    uint8_t outBuffer[61] {126,0,56,0,1,0,125,51,162,0,64,195,68,218,0,60,119,62,48,46,48,56,55,52,54,53,60,47,119,62,60,120,62,48,46,48,56,55,52,54,53,60,47,120,62,60,121,62,48,46,48,56,55,52,54,53,60,47,121,62, 27};
     
-    
-// packet 7E 00 47 00 01 00 7D 33 A2 00 40 C3 44 DA 00 3C 77 3E 30 2E 30 38 37 34 36 35 3C 2F 77 3E 3C 78 3E 30 2E 30 38 37 34 36 35 3C 2F 78 3E 3C 79 3E 30 2E 30 38 37 34 36 35 3C 2F 79 3E 1B
+// packet 7E 00 38 00 01 00 7D 33 A2 00 40 C3 44 DA 00 3C 77 3E 30 2E 30 38 37 34 36 35 3C 2F 77 3E 3C 78 3E 30 2E 30 38 37 34 36 35 3C 2F 78 3E 3C 79 3E 30 2E 30 38 37 34 36 35 3C 2F 79 3E 1B
     
     //beginning of packet
    // uint8_t outBuffer[76] {126,0,71,0,1,0,125,51,162,0,64,195,68,218,0};
@@ -77,11 +76,15 @@ void QuadOSCApp::update()
         osc::Message msg;
         mOscListener.getNextMessage(&msg);
 
-        if (msg.getNumArgs() == 5)
+        if (msg.getNumArgs() == 3)
         {
             std::string wStr (msg.getArgAsString(0, TRUE));
             std::string xStr (msg.getArgAsString(1, TRUE));
             std::string yStr (msg.getArgAsString(2, TRUE));
+            
+            console() << wStr << endl;
+            console() << xStr << endl;
+            console() << yStr << endl;
             
             QuadOSCApp::strToChars(wStr, 1);
             QuadOSCApp::strToChars(xStr, 2);
@@ -155,7 +158,7 @@ void QuadOSCApp::update()
             outBuffer[60] = QuadOSCApp::checkSum(outBuffer);
         }
         
-        console() << hex << outBuffer[60] << std::endl;
+     //   console() << hex << outBuffer[60] << std::endl;
         
         double now = getElapsedSeconds();
         double deltaTime = now - lastUpdate;
